cd920fb8c45a2c9d4a88a6ea7018cf79dc9ec25a
eliza <elizabeth.mrs.jack@gmail.com>
2024-07-08 16:38:38 +0300

    commit 5 (for jenkins -2 tests)

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -7,53 +7,6 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
-
-//    WebDriver driver;
-//
-//    public LoginPage(WebDriver driver) {
-//        this.driver = driver;
-//    }
-//
-//    public void clickLoginLink() {
-//        WebElement loginLink = driver.findElement(By.linkText("Вхід"));
-//        loginLink.click();
-//    }
-//
-//    public void enterEmail(String email) {
-//        WebElement emailField = driver.findElement(By.cssSelector("#loginform-login"));
-//        emailField.sendKeys(email);
-//    }
-//
-//    public void enterPassword(String password) {
-//        WebElement passwordField = driver.findElement(By.cssSelector("#loginform-password"));
-//        passwordField.sendKeys(password);
-//    }
-//
-//    public void clickLoginButton() {
-//        WebElement loginButton = driver.findElement(By.name("login-button"));
-//        loginButton.click();
-//    }
-//
-//    public boolean isUserProfileDisplayed() {
-//        WebElement userProfile = driver.findElement(By.xpath("//a[@href='https://cabinet.planetakino.ua']"));
-//        return userProfile.isDisplayed();
-//    }
-//
-//    public boolean isEmptyFieldErrorDisplayed() {
-//        WebElement emptyFieldError = driver.findElement(By.xpath("//p[text()='Необхідно заповнити це поле']"));
-//        return emptyFieldError.isDisplayed();
-//    }
-//
-//    public boolean isRegistrationFormDisplayed() {
-//        WebElement boxElement = driver.findElement(By.xpath("//div[@class='box']"));
-//        return boxElement.isDisplayed();
-//    }
-//
-//    public void clickRegisterLink() {
-//        WebElement registerLink = driver.findElement(By.linkText("Реєстрація"));
-//        registerLink.click();
-//    }
-
     private WebDriver driver;
 
     @FindBy(linkText = "Вхід")
